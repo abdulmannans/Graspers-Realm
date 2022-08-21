43d02a870689748bc58704494b165361b5fc3969
abdulmannans <samannan1999@gmail.com>
2022-08-22 00:17:03 +0530

    Formatted Code

--- abdulmannans_Assignment_1/src/AbdulMannanSAssignment1.java
+++ abdulmannans_Assignment_1/src/AbdulMannanSAssignment1.java
@@ -1,27 +1,33 @@
 public class AbdulMannanSAssignment1 {
-    public static String discountEligibility(int [] totalInvoices){
-        if(totalInvoices.length > 4){
-            int sumOfAllInvoices = 0;
-            int discountedAmount;
-
-            for (int totalInvoice : totalInvoices) {
-                sumOfAllInvoices += totalInvoice;
-            }
-            
-            discountedAmount = sumOfAllInvoices * 10/100;
-            return "Get Flat " + discountedAmount + "Rs Off on Next Order.";
+    private static String discountEligibility(int[] totalInvoices) {
+        if (totalInvoices.length > 4) {
+            return eligibleUser(totalInvoices);
         }
         return "Not Eligible For Discount.";
     }
+
+    private static String eligibleUser(int[] totalInvoices) {
+        int sumOfAllInvoices = 0;
+        for (int totalInvoice : totalInvoices) {
+            sumOfAllInvoices += totalInvoice;
+        }
+        return "Get Upto " + getDiscountAmount(sumOfAllInvoices) + "Rs Off on Next Order.";
+    }
+
+    private static void getResults(int[] totalInvoices, String customerName) {
+        System.out.println(customerName + "'s Eligibility:");
+        System.out.println(discountEligibility(totalInvoices) + "\n");
+    }
+
+    private static int getDiscountAmount(int amount) {
+        return amount * 10 / 100;
+    }
+
     public static void main(String[] args) {
-        int[] abdulInvoices = new int[]{100,232,300};
-        int[] ibrahimInvoices = new int[]{321,120,70,500,379};
-
-        System.out.println("Abdul's Eligibility:");
-        System.out.println(discountEligibility(abdulInvoices));
-
-        System.out.println("\nIbrahim Eligibility:");
-        System.out.println(discountEligibility(ibrahimInvoices));
+        int[] abdulInvoices = new int[]{100, 232, 300};
+        int[] ibrahimInvoices = new int[]{321, 120, 70, 500, 379};
+        getResults(abdulInvoices, "Abdul");
+        getResults(ibrahimInvoices, "Ibrahim");
     }
 
 }
